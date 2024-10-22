2a9dcef29cce8da174efe895384c4187d3081fbf
wingyou <winwing.you@gmail.com>
2024-10-23 00:12:34 +0900

    test(Car): add move and print test

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+/**
+ * Car
+ */
+public class Car {
+
+    private String name;
+    private int progress;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public void moveOrStop() {
+        // TODO: implement method
+    }
+
+    @Override
+    public String toString() {
+        // TODO: implement method
+        return "";
+    }
+
+    public String getName() {
+        // TODO: implement method
+        return null;
+    }
+
+    public int getProgress() {
+        // TODO: implement method
+        return 0;
+    }
+}
