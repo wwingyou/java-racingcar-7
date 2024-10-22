03c12c88a09f253f65e573d69c0dc212bcfd024a
wingyou <winwing.you@gmail.com>
2024-10-23 02:16:19 +0900

    feat(StaticRandomGenerator): add constructor with staticNumber

--- src/main/java/racingcar/StaticRandomGenerator.java
+++ src/main/java/racingcar/StaticRandomGenerator.java
@@ -7,6 +7,12 @@ public class StaticRandomGenerator implements RandomGenerator {
 
     private int staticNumber;
 
+    public StaticRandomGenerator() {}
+
+    public StaticRandomGenerator(int staticNumber) {
+        this.staticNumber = staticNumber;
+    }
+
     @Override
     public int getRandomNumber() {
         return staticNumber;
